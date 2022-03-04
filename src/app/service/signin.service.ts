592a5413ed85161862c332bdfd0d130228f72d58
silas-sousa <silassousadejesus@gmail.com>
2022-03-04 09:50:59 -0300

    preparando para fazer pr

---
+++ src/app/service/signin.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router';
+import { Signin } from '../modelInterface/signin';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SigninService {
+
+  private url = "http://localhost:3000/user"
+
+
+  constructor(private http: HttpClient,
+              private router: Router) { }
+
+
+  signIn(user:Signin){
+    return this.http.post<Signin>(this.url + '/signin', user)
+  }
+
+}
