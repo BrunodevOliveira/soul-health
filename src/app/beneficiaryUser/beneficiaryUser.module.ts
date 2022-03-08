5ea5eb7b0932005af01dd54dbfc28f1103423f00
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-03-08 09:17:11 -0300

    Integração do DialogFlow com página home

--- src/app/beneficiaryUser/beneficiaryUser.module.ts
+++ src/app/beneficiaryUser/beneficiaryUser.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../app-shared/shared-module/shared.module';
 import { BeneficiaryUserRoutingModule } from './beneficiaryUser-routing.module';
+import { HomeUserComponent } from './homeUser/homeUser.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeUserComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
     BeneficiaryUserRoutingModule
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class BeneficiaryUserModule { }
