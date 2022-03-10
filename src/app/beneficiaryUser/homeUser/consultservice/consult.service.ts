39ea7d5384ffef5d7f1a00e21335986be897f7ac
silas-sousa <silassousadejesus@gmail.com>
2022-03-10 13:20:32 -0300

    informações de consulta

--- src/app/beneficiaryUser/homeUser/consultservice/consult.service.ts
+++ src/app/beneficiaryUser/homeUser/consultservice/consult.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultService {
 
-  constructor() { }
+  url = 'http://localhost:3000/consult'
+
+  constructor(private http: HttpClient,
+             private router: Router) { }
+
+  getconsult(email:string){
+    return this.http.get(`${this.url}/showconsults/${email}`)
+  }
+
+
 }
