5388471270363e722ad0e165cffda78c2bda09eb
Lucélia Batista <lucelia.ti@gmail.com>
2022-03-16 08:38:20 -0300

    Subpasta 'serviços' removida da pasta doctors, por não ter sido necessária ao projeto

--- src/app/doctors/doctors.module.ts
+++ src/app/doctors/doctors.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { DoctorsRoutingModule } from './doctors-routing.module';
 import { AccreditedComponent } from './accredited/accredited.component';
-import { ServiceComponent } from './service/service.component';
 
 
 @NgModule({
   declarations: [
     AccreditedComponent,
-    ServiceComponent
   ],
   imports: [
     CommonModule,
